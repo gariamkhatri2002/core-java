d6918628c03208add50baf164437cc1ed866b73e
Gaurav <khatrigarima073@gmail.com>
2024-08-28 11:25:19 +0530

    declare array

---
+++ array/Basicarray.java
@@ -0,0 +1,75 @@
+//HOW TO DECLARE ARRAY;
+public class Basicarray{
+    public static void main(String[] args){
+
+      // all primitive data type used
+
+      int a[]={1,3,2,5,8};
+      System.out.println(a[2]);
+      float b[]={2.2f,3.6f,4.5f,7.9f,8.5f};
+      System.out.println(b[3]);    
+      char c[]={'a','s','d','g','k'};
+      System.out.println(c[2]);
+      boolean d[]={true,false};
+      System.out.println(d[0]);
+      long e[]={123,4125,78965,1453};
+      System.out.println(e[2]);
+      double f[]={12.356,45.2365,12.3456};
+      System.out.println(f[1]);
+      byte g[]={45,12,78};
+      System.out.println(g[2]);
+      short h[]={123,456,789,147};
+      System.out.println(h[3]);
+      
+
+      /*var used
+       var a=new Object[]{
+          23,2.3f,'u',45.9f,"regex"};
+        System.out.println(a[2]);
+        
+      */
+
+
+      /*with Object class
+
+      Object a[]={12,45.6f,'k',"software",true,123.45698};
+      System.out.println(a[5]);
+      */
+
+
+      /*//with String
+
+      String a[]={"regex","komal","car","123","Rj14 ML0819"};
+      System.out.println(a[4]);
+      */
+
+
+      /*all wrapper class used 
+      Integer a[]={12,45,36,78};
+      System.out.println(a[1]);
+      Float b[]={2.5f,4.6f,47.2f};
+      System.out.println(b[1]);
+      Character c[]={'u',2,'o',45};
+      System.out.println(c[2]);
+      Long d[]={125l,48796l,236578l};
+      System.out.println(d[2]);
+      Double e[]={12.3654,456.2147,478.259};
+      System.out.println(e[0]);
+      Byte f[]={125,-110,78,-96};
+      System.out.println(f[3]);
+      Short g[]={1236,4789,5248};
+      System.out.println(g[1]);
+      Boolean h[]={true,false,false,true,true};
+      System.out.println(h[4]);
+      */
+
+    }
+}
+
+
+
+
+
+
+
+
