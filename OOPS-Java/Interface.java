8c80bd38f07e3d55e233317b1ef8c144395781aa
Gaurav <khatrigarima073@gmail.com>
2024-09-30 21:02:22 +0530

    interface in java

---
+++ OOPS-Java/Interface.java
@@ -0,0 +1,35 @@
+
+interface A{
+    int show(int a);             //inside the interface method by default abstract and public 
+    int print(String name);
+}
+interface C{
+    void show();
+    void print();
+}
+class B implements A,C{
+    public int show(int a){
+        System.out.print("interface A method show :");
+        return a;
+    }
+    public int print(String name){
+        System.out.println("interface A method print :"+name);
+        return 0;
+    }
+    public void show(){
+        System.out.println("interface B method show");
+    }
+    public void print(){
+        System.out.println("interface B method print");
+    }
+    
+}
+public class Interface {
+    public static void main(String[] args) {
+        B oc=new B();
+        System.out.println(oc.show(10));
+        oc.print("regex");
+        oc.show();
+        oc.print();
+    }
+}
