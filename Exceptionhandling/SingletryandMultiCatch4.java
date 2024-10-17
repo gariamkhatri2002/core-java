41c7d61ca42bb3a1e09bc2f821beac764561bc98
Gaurav <khatrigarima073@gmail.com>
2024-10-17 19:09:17 +0530

    single try with multiple catch

---
+++ Exceptionhandling/SingletryandMultiCatch4.java
@@ -0,0 +1,16 @@
+public class SingletryandMultiCatch4 {
+    public static void main(String[] args) {
+        try{
+            int a=6;
+            int b=0;
+            int c=a/b;
+            System.out.println(c);
+        }
+        catch(Exception ex){
+            System.out.println("exception handle");
+        }
+        catch(Throwable ex){
+            System.out.println("throwable handle");
+        }
+    }
+}
