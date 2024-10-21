48988ff23c86ecf7a83f767ea16767fab148a1ff
Gaurav <khatrigarima073@gmail.com>
2024-10-21 23:39:18 +0530

    number pattern

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner oc=new Scanner(System.in);
+        System.out.print("Enter the size:");
+        int size=oc.nextInt();
+        int num=1;
+        for(int i=1;i<=size;i++){
+            for(int j=1;j<=size;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
