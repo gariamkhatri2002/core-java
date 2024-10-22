aef38110bf5ebb14c6c4f31cc77245be0bb1060c
Gaurav <khatrigarima073@gmail.com>
2024-10-22 12:15:40 +0530

    use of yield() method

---
+++ Multithreading1/MethodInThread1.java
@@ -0,0 +1,20 @@
+package Mutlithreading1;
+
+class A extends Thread{
+    public void run(){                        //then run class A thread
+        for(int i=1;i<=3;i++){                //it is work as a TS(thread schedular)
+            Thread.yield();
+            System.out.println("parent thread ");
+        }
+    }
+}
+public class MethodInThread1 {
+    
+    public static void main(String[] args) {
+        A p=new A();
+        p.start();
+        for(int i=1;i<=3;i++){
+            System.out.println("main thread");       //first run main thread 
+        }
+    }
+}
