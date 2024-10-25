0316b2163827b1014143c995eac5e972958d6a0d
Gaurav <khatrigarima073@gmail.com>
2024-10-25 22:31:12 +0530

    alphabet pattern

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,10 @@
+public class Pattern12 {
+    public static void main(String[] args) {
+        for(char i='A';i<='E';i++){
+            for(char j='A';j<='E';j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
