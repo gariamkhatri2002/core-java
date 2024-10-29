8071766d8109c86d05ccbb8c32031d6b9408654c
Gaurav <khatrigarima073@gmail.com>
2024-10-29 23:37:02 +0530

    star pattern

---
+++ Patterns/Pattern17.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner oc=new Scanner(System.in);
+        System.out.print("Enter the size:");
+        int size=oc.nextInt();
+        for(int i=1;i<=size;i++){
+            for(int j=1;j<=size;j++){
+                if(i==j ||i==1 ||j==1){
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
