d87d7b6601cb252a6bd0cdafe46dc2b4e2698cec
Gaurav <khatrigarima073@gmail.com>
2024-11-10 23:12:56 +0530

    creation of String buffer

---
+++ String/StringBufferBasic.java
@@ -0,0 +1,13 @@
+//The StringBuffer class in Java is the same as String class except it is mutable i.e. it can be changed.
+public class StringBufferBasic {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("garima");
+        System.out.println(sb);
+        
+        //it can be changed
+        StringBuffer sc=new StringBuffer("komal");
+        sc.append(sb);
+        System.out.println(sc);
+        
+    }
+}
