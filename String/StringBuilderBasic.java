f3df973e40ea31bc12786eefd22575728e3a3a23
Gaurav <khatrigarima073@gmail.com>
2024-11-11 14:33:14 +0530

    creating string builder

---
+++ String/StringBuilderBasic.java
@@ -0,0 +1,24 @@
+// Java StringBuffer class is used to create mutable (modifiable) String objects. 
+public class StringBuilderBasic {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("garima");
+        System.out.println(sb);
+
+        StringBuilder sc=new StringBuilder("khatri");
+        
+        sb.append(sc);                                  //OBJECT VALUES CAN BE CHANGED
+        System.out.println(sb);
+        sb=sb.append("java");                       //object reassign
+        System.out.println(sb);
+        
+        final StringBuilder oc=new StringBuilder("komal");
+        oc.append("khatria");                       // change object value
+        System.out.println(oc);
+        
+        // oc=oc.append("java");                      //object is final we can not reassign the object value
+        // System.out.println(oc);
+
+
+
+    }
+}
