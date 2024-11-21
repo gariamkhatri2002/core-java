537feae959c9872c11b7bc27562bd924cf2b78f8
Gaurav <khatrigarima073@gmail.com>
2024-11-21 15:50:05 +0530

    find armstrong number in a given range

---
+++ Basic_ControlStatement/ArmstrongNumber.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the starting number:");
+        int start=sc.nextInt();
+        System.out.print("enter the ending number:");
+        int end=sc.nextInt();
+        for(int i=start;i<=end;i++){
+            int num=i;
+            int sum=0;
+            while(num>0){
+                sum=sum+(num%10)*(num%10)*(num%10);
+                num/=10;
+            }
+            if(sum==i){
+                System.out.print(sum+" ");
+            }
+        }
+    }
+}
