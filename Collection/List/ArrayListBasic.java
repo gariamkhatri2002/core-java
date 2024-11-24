f63997bd974e3b831e04c60cb42183a9e58f5fdb
Gaurav <khatrigarima073@gmail.com>
2024-11-24 23:54:04 +0530

    array list basics

---
+++ Collection/List/ArrayListBasic.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ArrayListBasic{
+    public static void main(String[] args) {
+        ArrayList al=new ArrayList<>();
+        al.add("hello");
+        al.add(39);
+        al.add("garima");
+        al.add(12.36);
+        System.out.println(al);
+    }
+}
