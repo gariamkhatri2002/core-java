6e106c2db3e0b50296297a52a9cc3f466ceb05bb
Gaurav <khatrigarima073@gmail.com>
2024-12-18 11:08:33 +0530

    create and print through different ways

--- Collection/Set/HashsetBasic.java
+++ Collection/Set/HashsetBasic.java
@@ -7,11 +7,17 @@ public class HashsetBasic {
         ll.add(20);
         ll.add(30);
         ll.add(40);
-
+        //does not contain duplicate values
+        ll.add(30);
         System.out.print(ll);
         System.out.println();
         for(int i:ll){
             System.out.print(i+" ");
+        }
+        System.out.println();
+        Iterator itr=ll.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next()+" ");
         }
     }
 }
