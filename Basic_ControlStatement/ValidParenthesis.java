df8ba8b72545ebb2c2100f8185de22e6207ce7f9
Gaurav <khatrigarima073@gmail.com>
2025-01-04 12:07:24 +0530

    count open and closing parenthesis

---
+++ Basic_ControlStatement/ValidParenthesis.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ValidParenthesis {
+    static void countparenthesis(String s){
+        char a[]=s.toCharArray();
+        int countclose=0;
+        int countopen=0;
+        for(int i=0;i<s.length();i++){
+            if(a[i]=='('){
+                countopen++;
+            }
+            if(a[i]==')'){
+                countclose++;
+            }
+        }
+        if(countclose==countopen){
+            System.out.print("valid parenthesis");
+        }
+        else{
+            System.out.print("not valid parenthesis");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner oc=new Scanner(System.in);
+        System.out.print("Enter string:");
+        String s=oc.nextLine();
+        countparenthesis(s);
+    }
+}
